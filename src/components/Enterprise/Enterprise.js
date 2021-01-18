1a5e81436a7c36b6370ba9033c818dd4f0468e3b
zrr <zrexrodriguez@gmail.com>
2021-01-18 12:47:32 -0800

    converted rem to px

--- src/components/Enterprise/Enterprise.js
+++ src/components/Enterprise/Enterprise.js
@@ -11,63 +11,62 @@ justify-content: center;
 export const Accent = styled.hr`
 display: block;
 unicode-bidi: normal;
-margin-block-start: 0rem;
-margin-block-end: 0rem;
-margin-inline-start: 0rem;
-margin-inline-end: 0rem;
+margin-block-start: 0px;
+margin-block-end: 0px;
+margin-inline-start: 0px;
+margin-inline-end: 0px;
 overflow: hidden;
 border-style: none;
-border-width: 0rem;
+border-width: 0px;
 
 width: 48px;
-height: 0.375rem;
+height: 6px;
 background: linear-gradient(90deg,#13ADC7, #945DD6);
-border-radius: 1.875rem;
-margin-bottom: 2rem;
+border-radius: 30px;
+margin-bottom: 32px;
 `
 
 export const Title = styled.h2`
 font-family: "Roobert TRIAL", Arial, sans-serif;
 font-style: normal;
 font-weight: 800;
-font-size: 3.5rem;
-line-height: 3.5rem;
+font-size: 56px;
+line-height: 56px;
 letter-spacing: 0.02em;
 
 background: -webkit-linear-gradient(121.57deg, #FFFFFF 18.77%, rgba(255, 255, 255, 0.66) 60.15%);
 -webkit-background-clip: text;
 -webkit-text-fill-color: transparent;
 
-margin-bottom: 1.125rem;
+margin-bottom: 18px;
 `
 
 export const Subtitle = styled.p`
 font-family: "Roobert TRIAL", Arial, sans-serif;
 font-style: normal;
 font-weight: normal;
-font-size: 1.5rem;
-line-height: 2.5rem;
+font-size: 24px;
+line-height: 40px;
 letter-spacing: 0.02em;
 color: rgba(255, 255, 255, 0.5);
-max-width: 50rem;
-margin-bottom: 5rem;
+max-width: 800px;
+margin-bottom: 80px;
 `
 
 export const ImageContainer = styled.div`
 text-align: center;
 background-image: radial-gradient(50% 50% at 50% 50%, rgba(79, 108, 176, 0.25) 53.8%, rgba(79, 108, 176, 0) 100%);
-
 `
 
 export const MainImage = styled.img`
-padding: 3.75rem;
+padding: 60px;
 `
 
 export const List = styled.ul`
 list-style-type: none;
 display: flex;
 justify-content: space-between;
-margin-top: 5.1875rem;
+margin-top: 83px;
 `
 
 export const ListContainer = styled.div`
@@ -79,19 +78,19 @@ export const ListTitle = styled.h4`
 font-family: "Roobert TRIAL", Arial, sans-serif;
 font-style: normal;
 font-weight: bold;
-font-size: 1.75rem;
-line-height: 2rem;
+font-size: 28px;
+line-height: 32px;
 letter-spacing: 0.02em;
 color: #FFFFFF;
-margin-bottom: 0.5rem;
+margin-bottom: 8px;
 `
 
 export const ListParagraph = styled.p`
 font-family: "Roobert TRIAL", Arial, sans-serif;
 font-style: normal;
 font-weight: normal;
-font-size: 1.125rem;
-line-height: 1.875rem;
+font-size: 18px;
+line-height: 30px;
 letter-spacing: 0.02em;
 color: rgba(255, 255, 255, 0.75);
 `
@@ -106,7 +105,7 @@ export const ListIcon = styled.i`
 display: block;
 width: 48px;
 height: 48px;
-margin-bottom: 0.5rem;
+margin-bottom: 8px;
 
 background-position: center;
 object-fit: scale-down; 
