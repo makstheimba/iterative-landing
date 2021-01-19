66eef382ca37f1f4c644ee66953940befe29da31
pinwei wu <pw.amy.wu@gmail.com>
2021-01-18 18:57:10 -0600

    add CommunityCard component

---
+++ src/components/CommunityCard/CommunityCard.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './CommunityCard.css';
+
+export default function CommunityCard({ title, description }) {
+  return (
+    <div className="communitycard">
+      <h3 className="communitycard__title">{title}</h3>
+      <p className="communitycard__description">{description}</p>
+    </div>
+  );
+}
+
+CommunityCard.propTypes = {
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+};
