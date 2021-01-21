fdaa4577bdf290c2f170b72c1aeb3387d2722b04
zrr <zrexrodriguez@gmail.com>
2021-01-21 14:00:11 -0800

    about us init

---
+++ src/components/Timeline/index.js
@@ -0,0 +1,12 @@
+import { Section, SectionTitle, SectionText } from '../GlobalStyles/index'
+import './Timeline.css'
+
+<Section>
+  <SectionTitle main>About Us</SectionTitle>
+  <SectionText>
+    Data Science = Highly Iterative Metrics-driven Process With Data and Code
+  </SectionText>
+  <div className="timeline__carousel-container">
+
+  </div>
+</Section>
