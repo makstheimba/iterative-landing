3d4f919ee4132e7323774c2ed329ff12c516aecc
enyelsequeira <enyelsequeira@hotmail.com>
2021-01-22 13:55:43 +0000

    career section done

---
+++ src/components/Career/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Section, SectionDivider, SectionSubText, SectionText, SectionTitle } from '../GlobalStyles'
+import { Aboutbutton } from './Career'
+
+const Career = () => {
+  return (
+    <Section>
+      <SectionTitle>
+        Career
+      </SectionTitle>
+      <SectionText>
+        Built with data scientists, ML engineers, and data engineers in mind.
+      </SectionText>
+      <SectionSubText>
+        Building contextual assistants & chatbots that really help customers is hard.Rasa provides infrastructure & tools necessary for high-performing, resilient,proprietary contextual assistants that work. With Iterative, all developers can create better text- and voice-based assistants.
+      </SectionSubText>
+      <Aboutbutton>Drop us a line</Aboutbutton>
+    </Section>
+  )
+}
+
+export default Career
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,12 @@
 import * as React from "react"
 import { Link } from "gatsby"
+import Career from "../components/Career"
 
 const AboutPage = () => {
     return (
         <div>
-            Hi! This is an About page.
-            <Link to="/">Go to the home page</Link>
+            <Career />
+
         </div>
     )
 }
