e326e6c50856723401a899746aed9686798f48a9
Marcos <hz.marcosib@gmail.com>
2021-01-22 19:16:42 -0800

    added Community component for desktop view

--- src/components/Community/Community.js
+++ src/components/Community/Community.js
@@ -1,8 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './Community.css';
+import twitter from '../../images/Twitter.svg'
+import discord from '../../images/Discord.svg'
+import github from '../../images/Github.svg'
 
 export default function Community() {
   return <div className="community">
+    <div className="community__accent"></div>
+    <h2 className="community__title">Meet our community</h2>
+    <ul className="community__list">
+      <li className="community__stat">
+        <p className="community__stat-title">200+</p>
+        <p className="community__stat-text">Open Source Contributors</p>
+     </li>
+     <li className="community__stat">
+       <p className="community__stat-title">1000+</p>
+       <p className="community__stat-text">Companies</p>
+     </li>
+     <li className="community__stat">
+        <p className="community__stat-title">4000+</p>
+        <p className="community__stat-text">Community Members</p>
+     </li>
+     <li className="community__stat">
+        <p className="community__stat-title">9000+</p>
+        <p className="community__stat-text">Github Stars</p>
+      </li>
+    </ul>
+    <div className="community__links">
+      <p className="community__text">Join the community:</p>
+      <img src={discord}/>
+      <img src={github}/>
+      <img src={twitter}/>
+    </div>
   </div>
 };
