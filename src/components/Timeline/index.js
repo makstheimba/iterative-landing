b1f02983c952403043005030ce145d7c23c17057
zrr <zrexrodriguez@gmail.com>
2021-01-22 18:54:29 -0800

    having trouble with js

--- src/components/Timeline/index.js
+++ src/components/Timeline/index.js
@@ -4,54 +4,91 @@ import { Link } from "gatsby"
 import TimelineImg from '../../images/timeline.svg'
 import './Timeline.css'
 
+
+const data = [
+  {
+    year: 2017,
+    text: "First version of DVC was created and open sourced. First users",
+  },
+  {
+    year: 2018,
+    text: "Iterative, Inc was incorporated. First hires",
+  },
+  {
+    year: 2019,
+    text: "More development on DVC. Adoption of the tool significantly increased",
+  },
+  {
+    year: 2020,
+    text: "DVC 1.0 was released. New product CML was released",
+  },
+  {
+    year: 2021,
+    text: "Viewer product release. First enterprise customers",
+  },
+]
+
+
+
+
 const Timeline = () => {
+const [currentPos, setcurrentPos] = React.useState(0);
+
+ const carouselRef = React.useRef();
+  const scrollerRef = React.useRef();
+
+  function handleScroll(){
+  setcurrentPos(carouselRef.current.scrollLeft);
+  findSetActive();
+}
+
+function findSetActive(){
+  //actually it would be better if these elements were created and inserted at the beggining
+  //so that the array would only be created once
+  const carouselItems = Array.from(carouselRef.querySelectorAll('.carousel__item'));
+  carouselItems.forEach(item =>{
+    if(currentPos >= item.offset.left){
+      item.classList.add('carousel__button_active');
+    }
+  })
+}
+
+  function handleClick(index){
+    const elemPos = carouselRef.current.querySelectorAll('.carousel__item')[index].offset.left;
+    scrollerRef.current.scrollTo({
+      left: elemPos,
+      behavior: 'smooth'
+    })
+  }
+
   return (
   <Section>
     <SectionTitle main>About Us</SectionTitle>
     <SectionText>
       Data Science = Highly Iterative Metrics-driven Process With Data and Code
     </SectionText>
-    <ul className="carousel__container">
-      
-      <li className="carousel__item" id="carousel__item-1">
-        <h1 className="carousel__item-title">2017<img className="carousel__item-img" src={TimelineImg} /></h1>
-        <p className="carousel__item-text"> First version of DVC was created and open sourced. First users
-        </p>
-    </li>
 
-      
-          <li className="carousel__item" id="carousel__item-2">
-        <h1 className="carousel__item-title">2018<img className="carousel__item-img" src={TimelineImg} /></h1>
-        <p className="carousel__item-text"> Iterative, Inc was incorporated. First hires
-        </p>
-      </li>
-      
-          <li className="carousel__item" id="carousel__item-3">
-        <h1 className="carousel__item-title">2019<img className="carousel__item-img" src={TimelineImg} /></h1>
-        <p className="carousel__item-text"> More development on DVC. Adoption of the tool significantly increased
-        </p>
-      </li>
-      
-              <li className="carousel__item" id="carousel__item-4">
-        <h1 className="carousel__item-title">2020<img className="carousel__item-img" src={TimelineImg} /></h1>
-        <p className="carousel__item-text"> DVC 1.0 was released. New product CML was released
-        </p>
-      </li>
-      
-                  <li className="carousel__item" id="carousel__item-5">
-        <h1 className="carousel__item-title">2021<img className="carousel__item-img" src={TimelineImg} /></h1>
-        <p className="carousel__item-text"> Viewer product release. First enterprise customers
-        </p>
-      </li>
-      
+    
+    <ul className="carousel__container" ref={carouselRef} onScroll={handleScroll}>
+    {data.map((item, index) => {
+          return (
+            <li key={index} className="carousel__item" id={`carousel__item-${index}`}>
+              <h1 className="carousel__item-title">{`${item.year}`}
+                <img className="carousel__item-img" src={TimelineImg}/>
+              </h1>
+              <p className="carousel__item-text">{item.text}</p>
+            </li>
+          )
+        })}
     </ul>
 
-    <div className="carousel__buttons">
-      <Link className="carousel__button" href="#carousel__item-1"></Link>
-      <Link className="carousel__button" href="#carousel__item-2"></Link>
-      <Link className="carousel__button" href="#carousel__item-3"></Link>
-      <Link className="carousel__button" href="#carousel__item-4"></Link>
-      <Link className="carousel__button" href="#carousel__item-5"></Link>
+    <div className="carousel__buttons" ref={scrollerRef}>
+    {data.map((item, index) => {
+          return (
+            // <Link key={index} className="carousel__button" href={`#carousel__item-${index}`}></Link>
+            <Link key={index} className="carousel__button" onClick={handleClick(index)}></Link>
+            )
+        })}
     </div>
   </Section>
   )
