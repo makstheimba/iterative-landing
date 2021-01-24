ce2425e258d6b8ba5ae38c789da7053033a35d0e
zrr <zrexrodriguez@gmail.com>
2021-01-23 23:32:51 -0800

    created leadership component

--- src/components/Leadership/index.js
+++ src/components/Leadership/index.js
@@ -3,11 +3,11 @@ import { Section, SectionTitle, SectionDivider } from '../GlobalStyles'
 import './Leadership.css'
 
 //images
+import avatarImg0 from '../../images/avatar/avatar-00.png'
 import avatarImg1 from '../../images/avatar/avatar-01.png'
 import avatarImg2 from '../../images/avatar/avatar-02.png'
 import avatarImg3 from '../../images/avatar/avatar-03.png'
 import avatarImg4 from '../../images/avatar/avatar-04.png'
-import avatarImg5 from '../../images/avatar/avatar-05.png'
 import avatarBgImgBlue from '../../images/avatar/avatar-bg-blue.svg'
 import avatarBgImgRed from '../../images/avatar/avatar-bg-red.svg'
 import avatarBgImgPurple from '../../images/avatar/avatar-bg-purple.svg'
@@ -15,10 +15,29 @@ import avatarBgImgPurple from '../../images/avatar/avatar-bg-purple.svg'
 
 const data = [
   {
-    img: avatarImg1,
+    img: avatarImg0,
     title: "Dmitry Petrov",
     text: "Creator of DVC. Ex-Data Scientist at Microsoft. PhD in Computer Science.",
-    color: avatarBgImgBlue
+  },
+  {
+    img: avatarImg1,
+    title: "Ivan Shcheklein",
+    text: "Ex co-founder of TweetedTimes - acquired by Yandex. Contributor to Sedna Database.",
+  },
+  {
+    img: avatarImg2,
+    title: "Elle O'Brien",
+    text: "Quantitative researcher. PhD in Neuroscience. Deep learning and bot enthusiast. Lecturer at University of Michigan.",
+  },
+  {
+    img: avatarImg3,
+    title: "Puneet Agarwal",
+    text: "Partner at True Ventures. Focused primarily on infrastructure, open-source and applications.",
+  },
+  {
+    img: avatarImg4,
+    title: "Will Freiberg",
+    text: "COO at Mesosphere. Technology executive, strategic operator and entrepreneurial leader.",
   },
 ]
 
@@ -31,8 +50,6 @@ const Leadership = () => {
         return (
           <div key={index}>
             <img src={item.img}/>
-            
-            <img src={item.color}/>
             <h4>{item.title}</h4>
             <p>{item.text}</p>
           </div>
