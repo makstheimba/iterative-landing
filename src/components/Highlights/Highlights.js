aed29eb26fdc3c2c96e75cef3a9a9d5e3b6a03ef
Marcos <hz.marcosib@gmail.com>
2021-01-24 14:34:43 -0800

    mobile and tablet views for community and highlights components

--- src/components/Highlights/Highlights.js
+++ src/components/Highlights/Highlights.js
@@ -9,18 +9,18 @@ export default function Highlights() {
   return <ul className="highlights">
     <li className="highlights__feature">
       <img src={gear} className="highlights__icon"/>
-      <h2 className="highlights__tite">Familiar</h2>
-      <p className="highlights__text">Don’t reinvent the wheel! Fast and cost-efficient path to production.</p>
+      <div className="highlights__text-container"> <h2 className="highlights__title">Familiar</h2>
+      <p className="highlights__text">Don’t reinvent the wheel! Fast and cost-efficient path to production.</p> </div>
     </li>
     <li className="highlights__feature">
       <img src={code} className="highlights__icon"/>
-      <h2 className="highlights__tite">Open Source</h2>
-      <p className="highlights__text">Lightweight. Open-source. No-strings attached.</p>
+      <div className="highlights__text-container"> <h2 className="highlights__title">Open Source</h2>
+      <p className="highlights__text">Lightweight. Open-source. No-strings attached.</p> </div>
     </li>
     <li className="highlights__feature">
       <img src={shield} className="highlights__icon"/>
-      <h2 className="highlights__tite">Secure</h2>
-      <p className="highlights__text">You data is always stored by you. Your models are trained on your machines.</p>
+      <div className="highlights__text-container"> <h2 className="highlights__title">Secure</h2>
+      <p className="highlights__text">You data is always stored by you. Your models are trained on your machines.</p> </div>
     </li>
   </ul>
 };
