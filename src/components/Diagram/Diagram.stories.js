390e8d58edeee8cbdbe7f5b381ee1151fc05ce31
julieg18 <juliannagal18@gmail.com>
2021-01-25 06:50:01 -0600

    feat: add Diagram component

---
+++ src/components/Diagram/Diagram.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Diagram from './Diagram';
+
+export const Template = () => <Diagram />;
+
+const MainDiagram = {
+  title: 'Main/Diagram',
+  component: Diagram,
+};
+export default MainDiagram;
