7af42804a4617cc7194f9c2d7d1914cc1560e892
julieg18 <juliannagal18@gmail.com>
2021-01-25 18:42:26 -0600

    feat: Add TimeLine component

---
+++ src/components/TimeLine/TimeLine.stories.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import TimeLine from './TimeLine';
+
+export const Template = (args) => <TimeLine {...args} />;
+
+const AboutTimeLine = {
+  title: 'About/TimeLine',
+  component: TimeLine,
+};
+export default AboutTimeLine;
+
+export const DefaultTimeLine = Template.bind({});
+DefaultTimeLine.args = {
+  times: [
+    {
+      title: '2017',
+      subtitle:
+        'First version of DVC was created and open sourced. First users.',
+    },
+    { title: '2018', subtitle: 'Iterative, Inc was incorporated. First hires' },
+    {
+      title: '2019',
+      subtitle:
+        'More development on DVC. Adoption of the tool significantly increased',
+    },
+    {
+      title: '2020',
+      subtitle: 'DVC 1.0 was released. New product CML was released',
+    },
+    {
+      title: '2021',
+      subtitle: 'Viewer product release. First enterprise customers',
+    },
+  ],
+};
