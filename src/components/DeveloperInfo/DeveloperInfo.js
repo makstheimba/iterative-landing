655dd4ec9fbc766ed5ad40c471888e45da6d2a9b
pinwei wu <pw.amy.wu@gmail.com>
2021-01-26 15:21:26 -0600

    add DeveloperInfo component

---
+++ src/components/DeveloperInfo/DeveloperInfo.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './DeveloperInfo.css';
+import Avatar from '../Avatar/Avatar';
+
+export default function DeveloperInfo({ developers = [] }) {
+  return (
+    <ul className="developerInfo">
+      {developers.map((developer, i) => (
+        <li className="developerInfo__list-item">
+          <Avatar
+            image={developer.image}
+            color={developer.color}
+            shape="square"
+          />
+          <div className="developerInfo__text-container">
+            <h4 className="developerInfo__name">{developer.name}</h4>
+            <p className="developerInfo__description">
+              {developer.description}
+            </p>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+}
