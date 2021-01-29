1795b82cf961d29df39aa0b5b4d4c99f6d32c100
pinwei wu <pw.amy.wu@gmail.com>
2021-01-29 15:30:45 -0600

    add children link to pricing card list

--- src/components/PricingCardList/PricingCardList.js
+++ src/components/PricingCardList/PricingCardList.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import './PricingCardList.css';
 import PricingCard from '../PricingCard/PricingCard';
+import Icon from '../Icon/Icon';
 import Button from '../Button/Button';
+import cmlIcon from '../../images/icn-cml.svg';
+import dvcIcon from '../../images/icn-dvc.svg';
+import viewerIcon from '../../images/icn-viewer.svg';
 
 export default function PricingCardList() {
   return (
@@ -11,9 +15,28 @@ export default function PricingCardList() {
         subtitle="Individuals and teams"
         description="DVC, CML, our other open-source tools come for free, no strings attached"
         children={
-          <Button type="orange-gradient" size="default">
-            Contact Us
-          </Button>
+          <ul className="pricing-card-list__link-container">
+            <li className="pricing-card-list__link">
+              <Icon
+                icon={cmlIcon}
+                link="about:blank"
+                linkDescription="Go to CML"
+                widthPixelSizes={[24, 24, 32]}
+                background="light"
+              />
+              <p className="pricing-card-list__link-text left"> Go to CML</p>
+            </li>
+            <li className="pricing-card-list__link">
+              <Icon
+                icon={dvcIcon}
+                link="about:blank"
+                linkDescription="Go to DVC"
+                widthPixelSizes={[24, 24, 32]}
+                background="light"
+              />
+              <p className="pricing-card-list__link-text"> Go to DVC</p>
+            </li>
+          </ul>
         }
       />
       <PricingCard
@@ -21,9 +44,16 @@ export default function PricingCardList() {
         subtitle="Visualisation and team collaboration "
         description="Studio is an extension of GitHub, GitLab or BitBucket. Sign up for the online SaaS version or contact us to get on-premise installation"
         children={
-          <Button type="orange-gradient" size="default">
-            Contact Us
-          </Button>
+          <div className="pricing-card-list__link-viewer">
+            <Icon
+              icon={viewerIcon}
+              link="about:blank"
+              linkDescription="Go to  Studio"
+              widthPixelSizes={[24, 24, 32]}
+              background="light"
+            />
+            <p className="pricing-card-list__link-text"> Go to Studio</p>
+          </div>
         }
       />
       <PricingCard
