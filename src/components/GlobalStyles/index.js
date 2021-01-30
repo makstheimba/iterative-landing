5901a053e4949f28727fac18f8e0aa53e00062fd
zrr <zrexrodriguez@gmail.com>
2021-01-30 14:10:53 -0800

    added cookie window

--- src/components/GlobalStyles/index.js
+++ src/components/GlobalStyles/index.js
@@ -224,6 +224,12 @@ export const ButtonFront = styled.button`
     box-shadow: inset 0px 2px 1px rgba(46, 49, 55, 0.15), inset 0px 0px 4px rgba(20, 20, 55, 0.3);
   }
 
+  &:disabled{
+    background: linear-gradient(270deg, #00DBD8 0%, #B133FF 100%);
+    opacity: 0.5;
+    box-shadow: inset 0px 2px 1px rgba(46, 49, 55, 0.15), inset 0px 0px 4px rgba(20, 20, 55, 0.3);
+  }
+
   @media ${(props) => props.theme.breakpoints.md} {
     font-size: ${({ alt }) => alt ? '20px' : '16px'};
   }
