8bc7d74eb8eac96ac0facb84e31dc41225d528d5
pinwei wu <pw.amy.wu@gmail.com>
2021-01-29 20:47:21 -0600

    adjust layout

--- src/components/PricingCardList/PricingCardList.js
+++ src/components/PricingCardList/PricingCardList.js
@@ -12,60 +12,57 @@ export default function PricingCardList() {
     <ul className="pricing-card-list">
       <PricingCard
         title="Open-source"
-        subtitle="Individuals and teams"
+        subtitle="Individuals and teams &nbsp; &nbsp; &nbsp;"
         description="DVC, CML, our other open-source tools come for free, no strings attached"
-        children={
-          <ul className="pricing-card-list__link-container">
-            <li className="pricing-card-list__link">
-              <Icon
-                icon={cmlIcon}
-                link="about:blank"
-                linkDescription="Go to CML"
-                widthPixelSizes={[24, 24, 32]}
-                background="light"
-              />
-              <p className="pricing-card-list__link-text left"> Go to CML</p>
-            </li>
-            <li className="pricing-card-list__link">
-              <Icon
-                icon={dvcIcon}
-                link="about:blank"
-                linkDescription="Go to DVC"
-                widthPixelSizes={[24, 24, 32]}
-                background="light"
-              />
-              <p className="pricing-card-list__link-text"> Go to DVC</p>
-            </li>
-          </ul>
-        }
-      />
+      >
+        <ul className="pricing-card-list__link-container">
+          <li className="pricing-card-list__link">
+            <Icon
+              icon={cmlIcon}
+              link="about:blank"
+              linkDescription="Go to CML"
+              widthPixelSizes={[24, 24, 32]}
+              background="light"
+            />
+            <p className="pricing-card-list__link-text left"> Go to CML</p>
+          </li>
+          <li className="pricing-card-list__link">
+            <Icon
+              icon={dvcIcon}
+              link="about:blank"
+              linkDescription="Go to DVC"
+              widthPixelSizes={[24, 24, 32]}
+              background="light"
+            />
+            <p className="pricing-card-list__link-text"> Go to DVC</p>
+          </li>
+        </ul>
+      </PricingCard>
       <PricingCard
         title="SaaS"
         subtitle="Visualisation and team collaboration "
         description="Studio is an extension of GitHub, GitLab or BitBucket. Sign up for the online SaaS version or contact us to get on-premise installation"
-        children={
-          <div className="pricing-card-list__link-viewer">
-            <Icon
-              icon={viewerIcon}
-              link="about:blank"
-              linkDescription="Go to  Studio"
-              widthPixelSizes={[24, 24, 32]}
-              background="light"
-            />
-            <p className="pricing-card-list__link-text"> Go to Studio</p>
-          </div>
-        }
-      />
+      >
+        <div className="pricing-card-list__link-viewer">
+          <Icon
+            icon={viewerIcon}
+            link="about:blank"
+            linkDescription="Go to  Studio"
+            widthPixelSizes={[24, 24, 32]}
+            background="light"
+          />
+          <p className="pricing-card-list__link-text"> Go to Studio</p>
+        </div>
+      </PricingCard>
       <PricingCard
         title="Enterprise"
         subtitle="Cross-org collaboration"
         description="Extend your unstructured data storages with data compliance, data access control and SSO capabilities. Online as well as on-premise installation are available"
-        children={
-          <Button type="orange-gradient" size="large">
-            Contact Us
-          </Button>
-        }
-      />
+      >
+        <Button type="orange-gradient" size="large">
+          Contact Us
+        </Button>
+      </PricingCard>
     </ul>
   );
 }
