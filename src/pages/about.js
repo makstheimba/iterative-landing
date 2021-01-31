14a4bb54f698b5a38e377ff97df569ee4813f64f
julieg18 <juliannagal18@gmail.com>
2021-01-30 18:09:24 -0600

    feat: add Form to pages/about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import Layout from '../components/Layout/Layout';
 import Cookies from '../components/Cookies/Cookies';
 import Header from '../components/Header/Header';
@@ -7,12 +7,43 @@ import Footer from '../components/Footer/Footer';
 import Team from '../components/Team/Team';
 import LeaderShip from '../components/Leadership/Leadership';
 import Career from '../components/Career/Career';
+import RequestDemoForm from '../components/RequestDemoForm/RequestDemoForm';
 import { times } from '../utils/data';
 
-export default function AboutPage({ onClick }) {
+export default function AboutPage() {
+  const [isRequestDemoFormVisible, setIsRequestDemoFormVisible] = useState(
+    false
+  );
+
+  function closeRequestDemoForm() {
+    setIsRequestDemoFormVisible(false);
+    window.removeEventListener('keyup', handleEscPopupClose);
+  }
+
+  function handleEscPopupClose(e) {
+    if (e.key === 'Escape') {
+      closeRequestDemoForm();
+    }
+  }
+
+  function openRequestDemoForm() {
+    setIsRequestDemoFormVisible(true);
+    window.addEventListener('keyup', handleEscPopupClose);
+  }
+
+  function handleRequestDemoFormSubmit(data) {
+    console.log(data);
+    return Promise.resolve(data);
+  }
+
   return (
     <Layout>
       <Cookies />
+      <RequestDemoForm
+        visible={isRequestDemoFormVisible}
+        onClose={closeRequestDemoForm}
+        onSubmit={handleRequestDemoFormSubmit}
+      />
       <Header
         title="About Us"
         text="Data Science = Highly Iterative Metrics-driven Process With Data and Code"
@@ -21,7 +52,7 @@ export default function AboutPage({ onClick }) {
       </Header>
       <Team />
       <LeaderShip />
-      <Career onClick={onClick} />
+      <Career onClick={openRequestDemoForm} />
       <Footer />
     </Layout>
   );
